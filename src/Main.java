1c89cca9036f81bc2f52750c51fa7e068a5c3c19
Terence Svubure <tsvubure@gmail.com>
2024-03-01 19:44:44 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,40 @@ public class Main {
 
         final int PERCENT = 100;
         final int MONTHS_IN_YEAR = 12;
+        double principal = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        double principal = scanner.nextDouble();
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Principal: ");
+            principal = scanner.nextDouble();
+            if (principal > 1_000 && principal < 1_000_000)
+                break;
+            System.out.println("Enter amount between $1,000 and $1,000,000");
+        }
 
-        System.out.print("Annual Interest rate: ");
-        float interestRate = scanner.nextFloat();
-        float monthlyInterest = interestRate / PERCENT / MONTHS_IN_YEAR;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Annual Interest rate: ");
+            float interestRate = scanner.nextFloat();
+            if (interestRate > 0 && interestRate <= 30) {
+                monthlyInterest = interestRate / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value greater than 0 but less than or equal to 30");
+        }
 
-        System.out.print("Period: ");
-        int period = scanner.nextInt();
-        int numberOfPayments = period * MONTHS_IN_YEAR;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Period: ");
+            int period = scanner.nextInt();
+            if (period > 1 && period < 30) {
+                numberOfPayments = period * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a number between 1 and 30");
+        }
 
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
